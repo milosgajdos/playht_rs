49484867e455d628a06e37cf21f00e1ec59843da
Milos Gajdos <milosthegajdos@gmail.com>
2024-04-03 18:37:58 +0100

    update: add cargo and lib.rs
    Signed-off-by: Milos Gajdos <milosthegajdos@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Play.ht RS
+//!
+//! An unofficial Play.ht API client library.
+//!
+//! Play.ht homesite: https://play.ht/
+//!
+//! Play.ht API docs: https://docs.play.ht/reference/api-getting-started
+//!
